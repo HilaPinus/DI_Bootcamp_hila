7a7388f139f3f87dd596b57d923460e7bbb0d304
hilapinus <hila.pinus@gmail.com>
2022-11-28 23:04:45 +0200

    dayli

---
+++ Week8/day1/Exercises/dayli/mycarousel/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import Slider from './Slide';
+function App() {
+  return (
+    <div className="App">
+      <Slider />
+    </div>
+  );
+}
+
+export default App;
